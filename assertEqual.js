bc2fb99fc227fadb8f2ad425f5fc797aa5600811
Alon Rodovinsky <alon.rodovinsky@gmail.com>
2021-01-04 21:38:28 +0000

    Assert function to test my code's vailidity

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected)
+    console.log("✅✅✅Assertion Passed: [actual] === [expected]");
+  if (actual !== expected)
+    console.log("🔴🔴🔴 Assertion Failed: [actual] !== [expected]");
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual(NaN, NaN);
+assertEqual(1, 1);
+assertEqual(1, 4);
