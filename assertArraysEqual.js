e3110d40d1339b9bfe9907eea872199fe42dcfa4
Alon Rodovinsky <alon.rodovinsky@gmail.com>
2021-01-05 21:39:02 +0000

    Took eqArray function outside of the other one

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,19 +1,19 @@
 "use strict";
 
-const assertArraysEqual = function (array1, array2) {
-  const eqArrays = function (array1, array2) {
-    // If the arrays are different lengths they're not identical
-    if (array1.length !== array2.length) return false;
+const eqArrays = function (array1, array2) {
+  // If the arrays are different lengths they're not identical
+  if (array1.length !== array2.length) return false;
 
-    for (let i = 0; i < array1.length; i++) {
-      // If there's ever an inequality the whole thing is false
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
+  for (let i = 0; i < array1.length; i++) {
+    // If there's ever an inequality the whole thing is false
+    if (array1[i] !== array2[i]) {
+      return false;
     }
-    return true;
-  };
+  }
+  return true;
+};
 
+const assertArraysEqual = function (array1, array2) {
   let result = eqArrays(array1, array2);
 
   if (result === true)
