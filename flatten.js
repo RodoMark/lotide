87960613fd3957db0731918381270ddc5b8287dd
Alon Rodovinsky <alon.rodovinsky@gmail.com>
2021-01-06 02:41:01 +0000

    Flattening with one level of nesting

---
+++ flatten.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const flatten = function (array) {
+  let flattenedArray = [];
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array[i].length; j++) {
+      flattenedArray.push(array[i][j]);
+    }
+  }
+  return flattenedArray;
+};
+
+console.log(
+  flatten([
+    [1, 2],
+    [3, 4],
+    [5, 6],
+    [7, 8],
+  ])
+);
+
+console.log(
+  flatten([
+    [1, "cat", "dog"],
+    [3, 4, "mouse", 5, 6, 77777],
+    [5, 6, "squirell", "horse"],
+    [7, 8, "mountain"],
+    ["beluga"],
+  ])
+);
