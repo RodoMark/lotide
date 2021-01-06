2ed7651ec030d8a53d3c0e81439a643ce3f1f5ae
Alon Rodovinsky <alon.rodovinsky@gmail.com>
2021-01-06 20:00:59 +0000

    Realized I was using non-index values, gotta start from 0git add middle.js !

--- middle.js
+++ middle.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const eqArrays = function(array1, array2) {
+const eqArrays = function (array1, array2) {
   // If the arrays are different lengths they're not identical
   if (array1.length !== array2.length) return false;
 
@@ -13,7 +13,7 @@ const eqArrays = function(array1, array2) {
   return true;
 };
 
-const assertArraysEqual = function(array1, array2) {
+const assertArraysEqual = function (array1, array2) {
   if (eqArrays(array1, array2)) {
     console.log(`✅✅✅Assertion Passed: ${array1} === ${array2}`);
   } else {
@@ -22,30 +22,30 @@ const assertArraysEqual = function(array1, array2) {
 };
 
 // I built this function to automate my testing criteria
-let sequenceBuilder = function(number, iterator) {
+let sequenceBuilder = function (number, iterator) {
   let array = [];
   for (let i = 0; i < number; i += iterator) array.push(i);
   return array;
 };
 
-const middle = function(array) {
+const middle = function (array) {
   let middleArray = [];
   // If array has no middle return blank array
   if (array.length <= 2) return [];
   // If the array is even
   if (array.length % 2 === 0) {
     // Push the two middle values into a new array
-    middleArray.push(Math.ceil((array.length - 1) / 2));
-    middleArray.push(Math.ceil(array.length / 2) + 1);
+    middleArray.push(Math.floor((array.length - 1) / 2));
+    middleArray.push(Math.floor(array.length / 2));
     // If the array is odd push the single middle value
-  } else middleArray.push(Math.ceil(array.length / 2));
+  } else middleArray.push(Math.floor(array.length / 2));
   return middleArray;
 };
 
 assertArraysEqual([], middle([1, 2]));
 assertArraysEqual([], middle([49, 50]));
-assertArraysEqual([3], middle([1, 2, 3, 4, 5]));
-assertArraysEqual([3, 4], middle([1, 2, 3, 4, 5, 6]));
-assertArraysEqual([4, 5], middle([1, 2, 3, 4, 5, 6, 7, 8]));
-assertArraysEqual([25, 26], middle(sequenceBuilder(50, 1)));
-assertArraysEqual([50], middle(sequenceBuilder(99, 1)));
+assertArraysEqual([2], middle([1, 2, 3, 4, 5]));
+assertArraysEqual([2, 3], middle([1, 2, 3, 4, 5, 6]));
+assertArraysEqual([3, 4], middle([1, 2, 3, 4, 5, 6, 7, 8]));
+assertArraysEqual([24, 25], middle(sequenceBuilder(50, 1)));
+assertArraysEqual([49], middle(sequenceBuilder(99, 1)));
