78aa25e7145038d9727375e4df0d394e3ea71e49
Alon Rodovinsky <alon.rodovinsky@gmail.com>
2021-01-06 18:42:23 +0000

    Counts letters like a champ

---
+++ countLetters.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected)
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  if (actual !== expected)
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const countLetters = function(string) {
+  const results = {};
+  // Needs to be lower case so there's only one instance of each letter
+  const lowerCase = string.toLowerCase();
+  // These are characters we don't need
+  const nonLetters = [" ", ",", ".", "?", "!", "(", ")"];
+
+  for (const letter of lowerCase) {
+    if (results[letter]) {
+      results[letter] += 1;
+    } else {
+      results[letter] = 1;
+    }
+  }
+
+  for (const character of nonLetters) {
+    delete results[character];
+  }
+  return results;
+};
+
+const result1 = countLetters("Hello from the other siiiiiiide!");
+
+console.log(assertEqual(result1["h"], 3));
+console.log(assertEqual(result1["i"], 7));
