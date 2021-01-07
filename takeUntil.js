59d5d59a693618677cef6e9e04e7a736324b9f63
Alon Rodovinsky <alon.rodovinsky@gmail.com>
2021-01-07 18:14:28 +0000

    Made the function stop once truthy value returned

---
+++ takeUntil.js
@@ -0,0 +1,55 @@
+"use strict";
+
+// The function will return a "slice of the array with elements taken from the beginning."
+// It should keep going until the callback/predicate returns a truthy value.
+
+const takeUntil = function(array, callback) {
+  const output = [];
+  for (let item of array) {
+    if (callback(item)) {
+      output.push(item);
+      return output;
+    }
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  // If the arrays are different lengths they're not identical
+  if (array1.length !== array2.length) return false;
+
+  for (let i = 0; i < array1.length; i++) {
+    // If there's ever an inequality the whole thing is false
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, (x) => x < 0);
+
+console.log("---");
+
+const data2 = [
+  "I've",
+  "been",
+  "to",
+  "Hollywood",
+  ",",
+  "I've",
+  "been",
+  "to",
+  "Redwood",
+];
+const results2 = takeUntil(data2, (x) => x === ",");
+assertArraysEqual(results1, [-1]);
+assertArraysEqual(results2, [","]);
