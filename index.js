43c8fccc255a3342d520ec7b4c11d0e411354ca5
Alon Rodovinsky <alon.rodovinsky@gmail.com>
2021-01-11 18:15:05 +0000

    Added an index with all lotide functions in it

---
+++ index.js
@@ -0,0 +1,41 @@
+const assertArraysEqual = "./assertArraysEqual";
+const assertEqual = "./assertEqual";
+const assertObjectsEqual = "./assertObjectsEqual";
+const buildSequence = "./buildSequence";
+const countLetters = "./countLetters";
+const countOnly = "./countOnly";
+const eqArrays = "./eqArrays";
+const eqObjects = "./eqObjects";
+const findKey = "./findKey";
+const findKeyByValue = "./findKeyByValue";
+const flatten = "./flatten";
+const head = "./head";
+const letterPositions = "./letterPositions";
+const map = "./map";
+const middle = "./middle";
+const tail = "./tail";
+const takeUntil = "./takeUntil";
+const without = "./without";
+
+const lotide = {
+  assertArraysEqual: "assertArraysEqual",
+  assertEqual: "assertEqual",
+  assertObjectsEqual: "assertObjectsEqual",
+  buildSequence: "buildSequence",
+  countLetters: "countLetters",
+  countOnly: "countOnly",
+  eqArrays: "eqArrays",
+  eqObjects: "eqObjects",
+  findKey: "findKey",
+  findKeyByValue: "findKeyByValue",
+  flatten: "flatten",
+  head: "head",
+  letterPositions: "letterPositions",
+  map: "map",
+  middle: "middle",
+  tail: "tail",
+  takeUntil: "takeUntil",
+  without: "without",
+};
+
+module.exports = lotide;
